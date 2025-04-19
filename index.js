aaa44d011affd1db86f7ca7c1eb7301c4420a939
mohsenXAD <mohsenxad@gmail.com>
2025-04-19 17:42:02 +0330

    change entry point of pacakge

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function ({ ESAJ_ACCESS_TOKEN }) {
+module.exports = async function ({ ESAJ_ACCESS_TOKEN }) {
   if (!ESAJ_ACCESS_TOKEN) {
     throw new Error("ESAJ Provider must have ESAJ_ACCESS_TOKEN");
   }
 
-  const useCases = require("./src")({
+  const useCases = await require("./src")({
     ESAJ_ACCESS_TOKEN,
   });
 
